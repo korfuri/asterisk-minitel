e4904b7580e850afeab78425690e5b6563e44f37
Uriel Corfa <korfuri@gmail.com>
2023-10-09 10:02:46 +0200

    Use the vendored app_softmodem.c in our flake.
    Also enable parallel building for asterisk.

--- flake.nix
+++ flake.nix
@@ -10,21 +10,15 @@
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [ ];
         };
-        packages.asterisk-softmodem-src = pkgs.fetchFromGitHub {
-          owner = "billsimon";
-          repo = "asterisk-Softmodem";
-          rev = "fc9c92249bc43657687d95a8b731ab8be158e687";
-          # sha256 = "sha256-SLh9bNhjYOKOOT34v8zlW+qiYEl1i0AHFrzSvGoBBDY=";
-          sha256 = "sha256-5NkvwI8v7Gi8GUflp2vajT79TK1EjMv1WeAKXoxLoOs=";
-        };
         packages.asterisk-softmodem = pkgs.asterisk.overrideAttrs (o: {
           buildInputs = o.buildInputs ++ [
             # packages.asterisk-softmodem-src
             pkgs.spandsp
           ];
           postPatch = o.postPatch + ''
-cp ${packages.asterisk-softmodem-src}/app_softmodem.c ./apps/app_softmodem.c
+cp ${./app_softmodem.c} ./apps/app_softmodem.c
 '';
+          enableParallelBuilding = true;
         });
       });
 }
