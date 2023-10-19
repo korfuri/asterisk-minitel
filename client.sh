10c08eed2f61871ac8529b2394732298bd22ab87
Uriel Corfa <korfuri@gmail.com>
2023-10-19 12:54:07 +0200

    client.sh: take an optional app name as argv[1]

--- client.sh
+++ client.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+APP="${1:-TEST}"
+
 (
     printf "Version: 1\r\nTXspeed: 133.333\r\nRXspeed: 8.33\r\n\r\n\x13S"  # ULM header
     sleep 0.2  # wait for the capabilities query
     printf "\x01aaa\x04"  # pretend to be the dumbest minitel
     sleep 0.5
-    printf "TESTPIL\x13\x41"  # TEST, ENVOI
+    printf "${APP?}\x13\x41"  # app name then, ENVOI
     sleep 0.2
 ) | nc -v 127.0.0.1 3615 | cat -e
