f9833bdd94f07b362bcd3ba0236ee1faf6caf2f3
Uriel Corfa <korfuri@gmail.com>
2024-04-26 15:32:42 +0200

    minitel.sh

--- minitel.sh
+++ minitel.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
-python3 -m minitel.main --assets_path minitel/assets --db_path sqlite:////tmp/minitel.db $*
+mkdir -p /tmp/uploads
+echo '*****'
+echo 'http://127.0.0.1:8080/e/index.html'
+echo '*****'
+echo ''
+python3 -m minitel.main --assets_path minitel/assets --db_path sqlite:////tmp/minitel.db --upload_path /tmp/uploads --web_port 8080 --ws_port 3611 $*
+
